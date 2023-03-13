c939238c71414ec4cd4adb2b5b66bee27072f660
jose.gamez.poza <jose.gamez.poza@nttdata.com>
2023-03-13 13:37:37 +0100

    Introducimos el problema de los códigos postales.

---
+++ problemaCodigosPostales/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Problema con los códigos postales");
+string cpMadrid = "28002";
+short.TryParse(cpMadrid, out var postalCodeMadrid);
+Console.WriteLine("El valor del código postal de Madrid es: "+postalCodeMadrid);
+
+
+string cpSevilla = "41011";
+short.TryParse(cpSevilla, out var postalCodeServilla);
+Console.WriteLine("El valor del código postal de Sevilla es: "+postalCodeServilla);
